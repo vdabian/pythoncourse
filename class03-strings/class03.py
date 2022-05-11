bc4df340d7b7bd4fde3be59bbaad8c98721db07e
Vinícius Dabian <vinicius_dabian@hotmail.com>
2022-05-10 21:34:59 -0300

    Class 03 - Strings

---
+++ class03-strings/class03.py
@@ -0,0 +1,16 @@
+"""
+str - string
+"""
+
+print('something')
+print("something")
+print('123456')  # '' String
+
+# print('This is a 'string' (str).')
+print("This is a 'string' (str).")
+print('This is a "string" (str).')
+print("This is a string \"string\" (str).")
+print('This is a string \'string\' (str).')
+print('This is a string \n (str).')
+print(r'This is a string \n (str).')
+print(r'This is a \'string\' (str).')
