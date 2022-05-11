2f0bf8d0e983d367ead9391a9d2877436b75c017
Vinícius Dabian <vinicius_dabian@hotmail.com>
2022-05-10 21:10:12 -0300

    Class 01 - Comments

---
+++ class01-comments/class01.py
@@ -0,0 +1,11 @@
+print('Line 1')  # Comment
+print('Line 2')  # Other comment
+# Comment about line 3
+"""
+print('Line 3')
+print('Line 4')  # Line 4
+"""
+# print('Line 5')
+print('Line 6')
+# Comment
+
