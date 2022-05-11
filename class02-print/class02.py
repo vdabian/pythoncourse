a79cef247c0c36c5faabdda850e5be74c7cbcc00
Vinícius Dabian <vinicius_dabian@hotmail.com>
2022-05-10 21:23:10 -0300

    Class 02 - Print

---
+++ class02-print/class02.py
@@ -0,0 +1,13 @@
+print(123456)
+print('Luiz', 'Otávio', '...')
+print('Luiz', 'Otávio', sep='-')
+print('João', 'e', 'Maria', sep='-')
+print('Luiz', 'Otávio', sep='-', end='###')
+print('João', 'e', 'Maria', sep='-', end='')
+print(' ')
+"""
+824.176.070-18
+"""
+print('824', '176', '070', sep='.', end='-')
+print('18')
+
